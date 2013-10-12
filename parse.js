ccda3d67f337bedfe1520a60da77968038b99c7f
Ismail <ismail@tesobe.com>
2013-10-12 21:10:59 +0200

    get_transaction scaffold added

--- parse.js
+++ parse.js
@@ -1,9 +1,19 @@
 function parse(query)
 { 
+	transactions = get_transactions();
+	if query.contains("how much money I"){
+		if query.contains("spent") {
+			// how much money I spent
+		}
+	}
     summary = "sum1";
     array = ["item1", "item2"];
     json = JSON.stringify({ summary: summary, transactions: array});
     return json;
+}
+
+function get_transactions(){
+	//Get transactions from Open Bank Project API
 }
 
 module.exports.parse = parse;
